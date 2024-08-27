f9172875e4f055c4950bb09e21fc955e8a325375
AliRezaMohammadiPY <13alirezamohammadi88@gmail.com>
2024-08-27 07:10:20 +0330

    Tamrin Mohit Masahat Uploaded

---
+++ jalase_3/03_mohit_masahat.py
@@ -0,0 +1,9 @@
+def mohit_masahat(tool, arz):
+    print("---------------------------------------\nmohit mostatil =", (tool + arz) * 2)
+    print("masahat mostatil =", tool * arz)
+
+tool = int(input("tool ra vared konid : "))
+arz = int(input("arz ra vared konid : "))
+
+mohit_masahat(tool, arz)
+print("------------------------------------------\nrooz khoobi dashte bashid khodahafez :)")
