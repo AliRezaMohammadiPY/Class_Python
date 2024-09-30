768022e844c9bce6fd262ee9c4e86c6b78dbabdc
AliRezaMohammadiPY <13alirezamohammadi88@gmail.com>
2024-09-30 18:00:20 +0330

    file none

--- jalase_6/07_Tavaan_2.py
+++ jalase_6/07_Tavaan_2.py
@@ -5,10 +5,14 @@ print("-----------------------------------------")
 tavaan = lambda num:num ** 2
 
 list_num = []
-voroodi = input("adad ra vared konid: ")
-voroodi = int(voroodi)
-list_num.append(voroodi)
-tavaan(voroodi)
-output = map (tavaan, list_num)
+while True:
+    voroodi = input("adad ra vared konid: ")
+    if voroodi == "end":
+        break
+    else:
+        voroodi = int(voroodi)
+        list_num.append(voroodi)
+        tavaan(voroodi)
+        output = map(tavaan, list_num)
 output = list(output)
-print(f"Javab Tavaan 2 Shoma : {output}")
+print(f"Javab Tavaan 2 haye Shoma : {output}")
